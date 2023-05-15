9ac651423c8597e46439a71a44543ccb7259bf58
Emre Çelen <79768707+emrcelen@users.noreply.github.com>
2023-05-15 10:32:41 +0300

    The account entity class has been added to the project.

---
+++ src/main/java/com/wenthor/urlshortener/model/Account.kt
@@ -0,0 +1,34 @@
+package com.wenthor.urlshortener.model
+
+import javax.persistence.*
+
+@Entity
+@Table(name = "account", schema ="url_shortener")
+data class Account(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="account_id")
+    val id: Long?,
+    @Column(name = "email",unique = true,nullable = false,)
+    val email: String?,
+    @Column(name="password", nullable = false)
+    val password: String?,
+    @OneToMany(mappedBy = "account", fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
+    val shortUrls: List<ShortUrl>?,
+    @OneToMany(mappedBy = "account", fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
+    val logShortUrls: List<LogShortUrl>?,
+    @ManyToOne(fetch = FetchType.EAGER, cascade = [CascadeType.PERSIST, CascadeType.MERGE])
+    @JoinColumn(name = "role_id", referencedColumnName = "role_id")
+    val role: Role?
+){
+    constructor(email: String,
+                password: String,
+                role: Role) : this(
+        null,
+        email = email,
+        password = password,
+        null,
+        null,
+        role = role
+    )
+}
