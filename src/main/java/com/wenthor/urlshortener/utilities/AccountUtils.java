26129f5e89d55ae2505942ae88acf70925955246
Emre Çelen <79768707+emrcelen@users.noreply.github.com>
2023-05-15 11:10:12 +0300

    A utility class has been created to determine the user associated with JWT-based requests.

---
+++ src/main/java/com/wenthor/urlshortener/utilities/AccountUtils.java
@@ -0,0 +1,37 @@
+package com.wenthor.urlshortener.utilities;
+
+import com.wenthor.urlshortener.model.Account;
+import com.wenthor.urlshortener.security.service.JwtService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+
+import java.security.Key;
+import java.util.function.Function;
+
+
+public final class AccountUtils {
+
+    private static <T> T exportToken(String token, Function<Claims, T> claimsTFunction) {
+        final Claims claims = Jwts
+                .parserBuilder()
+                .setSigningKey(getKey()) // parse işlemi ederken secret_key değerin atamasını yapıyoruz.
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return claimsTFunction.apply(claims);
+    }
+    private static Key getKey() {
+        byte [] key = Decoders.BASE64.decode("SecretShortenerURL003AppKeySecretShortenerURL002AppKeySecretShortenerURL001AppKey");
+        return Keys.hmacShaKeyFor(key);
+    }
+    private static String getHeadersToken(HttpHeaders headers){
+       return headers.get(HttpHeaders.AUTHORIZATION).get(0).substring(7);
+    }
+    public static String findByAccountEmail(HttpHeaders headers){
+       return exportToken(getHeadersToken(headers), Claims::getSubject);
+    }
+}
